6f4c14efc812189ed25e1e83dd5182cb258a794d
Kamil Chmiel <kamil.chmiel@plentific.com>
2024-10-28 12:22:07 +0100

    Update dependencies

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -2,8 +2,8 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 Future<bool> isInternetAvailable() async {
   final connectivity = Connectivity();
-  final connectivityResult = await connectivity.checkConnectivity();
+  final connectivityResult = (await connectivity.checkConnectivity()).first;
   return connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi || 
+      connectivityResult == ConnectivityResult.wifi ||
       connectivityResult == ConnectivityResult.ethernet;
 }
